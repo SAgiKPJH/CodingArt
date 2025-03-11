b421bf00f03b0adc540f3436bbb2f1353658b487
SagiKPJH1021 <Juhyung1021@gmail.com>
2025-03-11 18:14:19 +0900

    art : add art

---
+++ Mountain/Mountain.cs
@@ -0,0 +1,39 @@
+﻿namespace Mountain;
+public sealed record Mt                       (     )                    ;
+public sealed record MtK2                     (      )                   ;
+public sealed record Vinson                   (     )                    ;
+public sealed record Elbrus                   (   )                      ;
+public sealed record MtMakalu                 (  )                       ;
+public sealed record MtAconcagua              ( )                        ;
+public sealed record MtMatterhorn             ()                         ;
+public sealed record MtKilimanjaro                                       ;
+public sealed record MtMountStHelens                                     ;
+public sealed record MtMountWhitney                                      ;
+public sealed record MtMountTorre                                        ;
+public sealed record MtGasherbrum                                        ;
+public sealed record MtAnnapurna                                         ;interface
+   IMt;public record MtMountShasta                                       ;interface
+    IMountain;record MtMountAconcagua      ()                            ;interface
+IMon;  public record MtMountTammany                                      ;interface
+IMount;public record MtMountMckinley                                     ;
+public sealed record MtMountSierraNevada   ()                            ;
+public sealed record MtMountPicoDeOrizaba  ( )                           ;
+public sealed record MtMountSaskatchewan  (  )                           ;
+public sealed record MtMountKosciuszko    (   )                          ;
+public sealed record MtMountMcKinley      (    ):IMon                    ;
+public sealed record MtMountBaldy         (     ):IMount                 ;
+public sealed record MtMontBlanc           (     ):IMount                ;
+public sealed record MtManaslu             (      ):IMon                 ;
+public sealed record MtChoOyu            (        ):IMt                  ;
+public sealed record MtLhotse            (        ){ }                   ;
+public sealed record MtDenali            (         ){ }                  ;
+public sealed record MtEverest           (        ){   }                 ;
+public sealed record MountFuji          (        ){     }                ;
+public sealed record Dhaulagiri         (      ){       }                ;
+public sealed record MtMountBaker        (    ) {      }                 ;
+public sealed record Kangchenjunga       (    )  {    }                  ;
+public sealed record MtMountHood          (   )  {  }                    ;
+public sealed record Himalaya             (   )    {}                    ;
+public sealed record Adams                 (  )                          ;
+public sealed record Meru                   ()                           ;
+public sealed record Etna                                                ;
